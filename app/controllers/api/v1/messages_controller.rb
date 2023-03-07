e4ce0f34f56117741134c57175a0e6e1c1c6da55
MattGomb <gombosonline@gmail.com>
2023-03-07 17:18:49 -0500

    Clean controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -8,36 +8,6 @@ class Api::V1::MessagesController < ApplicationController
     render json: @messages
   end
 
-  # GET /messages/1
-  def show
-    render json: @message
-  end
-
-  # POST /messages
-  def create
-    @message = Message.new(message_params)
-
-    if @message.save
-      render json: @message, status: :created, location: api_v1_post_path(@message)
-    else
-      render json: @message.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /messages/1
-  def update
-    if @message.update(message_params)
-      render json: @message
-    else
-      render json: @message.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /messages/1
-  def destroy
-    @message.destroy
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
